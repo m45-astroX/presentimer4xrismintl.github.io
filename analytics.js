9730af1a04283fe3c99a73216f0769161f67053e
m45-astroX <yu.2876.2346@gmail.com>
2025-10-19 21:08:50 +0900

    remake

--- analytics.js
+++ analytics.js
@@ -1,6 +1,6 @@
-document.write(
-  '<script async src="https://www.googletagmanager.com/gtag/js?id=G-LJR2R07CH9"></script>'
-);
+//document.write(
+//  '<script async src="https://www.googletagmanager.com/gtag/js?id=G-LJR2R07CH9"></script>'
+//);
 window.dataLayer = window.dataLayer || [];
 function gtag() {
   dataLayer.push(arguments);
